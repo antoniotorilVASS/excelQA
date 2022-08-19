e823d63418fb165a750b4cbc8c52430cb1d5fc5b
antonio.toril@vass.es <antonio.toril@vass.es>
2022-08-19 08:39:13 +0200

    Actualización para que antes de iniciar el test borre lo que hay en downloads

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require('cypress')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8080/login',
-    video: false
+    video: false,
+    trashAssetsBeforeRuns: true
   }
 })
