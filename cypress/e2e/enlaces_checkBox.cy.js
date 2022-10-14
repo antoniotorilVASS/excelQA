a2057f6b353b980d2eab4d03b6bc6be8ac3ac94f
antonio.toril@vass.es <antonio.toril@vass.es>
2022-10-14 13:15:22 +0200

    Complete test

--- cypress/e2e/enlaces_checkBox.cy.js
+++ cypress/e2e/enlaces_checkBox.cy.js
@@ -1,3 +1,14 @@
+// Hide fetch/XHR requests
+const app = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+  const style = app.document.createElement('style');
+  style.innerHTML =
+    '.command-name-request, .command-name-xhr { display: none }';
+  style.setAttribute('data-hide-command-log-request', '');
+
+  app.document.head.appendChild(style);
+}
+
 describe('Comprobar pestañas enlaces pantallas y box menú', () => {
     context('Conexión', () => {
         before('Comprobar pestañas enlaces pantallas', () => {
